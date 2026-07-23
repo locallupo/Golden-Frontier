76baccfd333f549d4f41bdbc14b618c7a66e6cbd
locallupo <DanielPJacob@protonmail.com>
2026-07-23 15:14:25 +0100

    Project Setup Added mineral textures

--- src/main/java/net/locallupo/goldenfrontier/GoldenFrontier.java
+++ src/main/java/net/locallupo/goldenfrontier/GoldenFrontier.java
@@ -9,19 +9,11 @@ import org.slf4j.LoggerFactory;
 
 public class GoldenFrontier implements ModInitializer {
 	public static final String MOD_ID = "golden-frontier";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Vote Golden Frontier for the 2026 CurseForge Modjam!");
 	}
 
 	public static Identifier id(String path) {
